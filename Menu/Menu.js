b8846346d250623f2ed31ecf75098c54923ca35e
MacBookPro <macbookpro@MacBookPros-MacBook-Pro.local>
2018-04-25 14:04:26 -0500

    menu start
    imported menu.less used jQuery to select menu and menu-button.

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,6 +1,6 @@
 //Using jQuery, select the menu element and the menu-button element
-const menu;
-const menuButton;
+const menu = $('.menu');
+const menuButton = $('.menu-button');
 
 //Create a callback that will be passed to the menuButton click event listener
 //Inside this callback a class should be either added or removed from menu
