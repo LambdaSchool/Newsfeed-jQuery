cf942e2d6990f2348076613a0a05c959ee163fd7
mr-burton <taylormade800@gmail.com>
2018-04-25 12:47:19 -0700

    using jquery to select menu and menu button elements

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,6 +1,8 @@
 //Using jQuery, select the menu element and the menu-button element
 const menu;
 const menuButton;
+$(".menu");
+$(".menu-button");
 
 //Create a callback that will be passed to the menuButton click event listener
 //Inside this callback a class should be either added or removed from menu
