738283c95e4374355731934c03022a021a23b42a
kammagik <kammagik@yahoo.com>
2018-04-25 16:24:37 -0700

    fixed article issue, part 2 completed

--- Article/Article.js
+++ Article/Article.js
@@ -1,5 +1,5 @@
 /* Using jQuery, select all the article elements */
-let articles = ${".article"};
+let articles = $(".article");
 
 
 class Article {
@@ -14,24 +14,23 @@ class Article {
       Using our new reference to the element, find the expand button within
       the element and set a new property on the Article class.
     */
-    this.expandButton = .find(".expandButton");
+    this.expandButton = this.element.find(".expandButton");
     /*
       Using our new reference to the expandButton, use jQuery to set the inner
       text on the button.
     */
-    this.expandButton.text("Lucas...");
+     this.expandButton.text("Click to Expand"); //doesn't show up??
     /*
       Set a click handler on the expand button (or article element), calling
       the expandArticle method.
     */
-    this.expandButton.click() => {
-      this.expandArticle;
-    }
-  }
-
-  expandArticle() {
+    this.element.click(() => {
+      this.expandArticle();
+    });
+}
+  expandArticle(){
     /* Using our reference to the article element, add or remove a class */
-    this.element.toggleClass('article-open');
+    this.element.toggleClass("article-open");
   }
 }
 
@@ -40,6 +39,6 @@ class Article {
   Within .map, we create a new instance of Article passing in each article element
   to the constructor
 */
-articles = articles.map((index, element); => {
-  return new Article(element);
+articles = articles.map((index, element) => {
+   new Article(element);
 });
