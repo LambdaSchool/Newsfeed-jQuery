89b37562a239dde15a6d224c85f986a3e8bafb19
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-04-25 18:45:28 -0400

    Added close button - stretch goal: added a close button that makes article disappear on click

--- Article/Article.js
+++ Article/Article.js
@@ -4,29 +4,14 @@ let articles = $('.article');
 class Article {
   /* The constructor will take an element as it's only argument */
   constructor(element) {
-    /* 
-      We need to make sure the element being passed to us is a jQuery element.
-      To do so, wrap the element in jQuery syntax. 
-    */
     this.element = $(element);
 
-    /* 
-      Using our new reference to the element, find the expand button within 
-      the element and set a new property on the Article class. 
-    */
     this.expandButton = this.element.find('.expandButton');
-
-    /* 
-      Using our new reference to the expandButton, use jQuery to set the inner
-      text on the button. 
-    */
     this.expandButton.text('Click to Expand');
-
-    /* 
-      Set a click handler on the expand button (or article element), calling 
-      the expandArticle method. 
-    */
     this.expandButton.click(() => {this.expandArticle()});
+
+    this.closeButton = this.element.find('.close');
+    this.closeButton.click(() => { this.closeArticle() });
   }
   expandArticle() {
     if(this.element.hasClass('article-open')){
@@ -50,6 +35,9 @@ class Article {
       this.element.toggleClass('article-open');
       this.expandButton.text('Click to Expand');
     });
+  }
+  closeArticle(){
+    this.element.remove();
   }
 }
 
