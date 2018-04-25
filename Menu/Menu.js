05b716c7af72ff53857d5aced81b3e1849b09f92
mr-burton <taylormade800@gmail.com>
2018-04-25 13:27:36 -0700

    added click function to menu.js

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,13 +1,16 @@
 //Using jQuery, select the menu element and the menu-button element
-const menu;
-const menuButton;
-$(".menu");
-$(".menu-button");
+const menu = $(".menu");
+const menuButton = $(".menu-button");
+
 
 //Create a callback that will be passed to the menuButton click event listener
 //Inside this callback a class should be either added or removed from menu
 const toggleMenu = () => {
-
+    $(".menu").toggleClass("menu--open");
 }
+$(".menu-button").click(toggleMenu);
 
 //Using jQuery, add a click event to the menuButton, passing it the toggleMenu function.
+$(".menu-button").click(function () {
+    alert("button has been clicked");
+});
