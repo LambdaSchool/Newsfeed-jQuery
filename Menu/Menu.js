81cfe2b3bc3b390ba7ad148037eebb74ff11cb9d
Chris Honda <christopher.honda92@gmail.com>
2018-04-25 15:36:56 -0700

    added an animation to the menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -10,3 +10,13 @@ let toggleMenu = () => {
 
 //Using jQuery, add a click event to the menuButton, passing it the toggleMenu function.
 $('.menu-button').click(toggleMenu);
+
+$( ".menu-button" ).click(function() {
+    $( ".menu" ).animate({
+      opacity: 0.40,
+    //   left: "+=50",
+    //   height: "toggle"
+    }, 2500, function() {
+      // Animation complete.
+    });
+  });
