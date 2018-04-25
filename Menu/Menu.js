8034ecec3257635223ac7a8e7010e1e631783589
Brandon Benefield <bsquared18@gmail.com>
2018-04-25 15:04:39 -0400

    create callback func and pass to click event "toggleMenu" func toggles the ".menu--open" class on ".menu" "toggleMenu" is passed to the "menuButton" click event

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,12 +2,11 @@
 const menu = $('.menu');
 const menuButton = $('.menu-button');
 
-console.log(menu, menuButton);
-
 //Create a callback that will be passed to the menuButton click event listener
 //Inside this callback a class should be either added or removed from menu
 const toggleMenu = () => {
-
+  menu.toggleClass('menu--open');
 }
 
 //Using jQuery, add a click event to the menuButton, passing it the toggleMenu function.
+menuButton.click(toggleMenu);
