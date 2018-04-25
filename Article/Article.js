0ca555cf5eb65ee2b5e58b76575b1b69692ebf75
kelly <kelfro@fastmail.com>
2018-04-25 14:34:00 -0700

    finished task 2
    added expand button that opens and closes articles

--- Article/Article.js
+++ Article/Article.js
@@ -1,6 +1,9 @@
 /* Using jQuery, select all the article elements */
-let articles;
+let articles = jQuery(".article");
 
+// map()
+
+console.log(articles);
 
 class Article {
   /* The constructor will take an element as it's only argument */
@@ -9,25 +12,30 @@ class Article {
       We need to make sure the element being passed to us is a jQuery element.
       To do so, wrap the element in jQuery syntax. 
     */
-    this.element = $();
+    this.element = $(element);
     /* 
       Using our new reference to the element, find the expand button within 
       the element and set a new property on the Article class. 
     */
-    this.expandButton;
+      this.expandButton = $(element).find(".expandButton").css({'color':'white','backgroundColor':'#388E3C','font-size':'1rem', 'padding':'2px'});
     /* 
       Using our new reference to the expandButton, use jQuery to set the inner
       text on the button. 
     */
+      this.expandButton.text("Expand");
 
     /* 
       Set a click handler on the expand button (or article element), calling 
       the expandArticle method. 
     */
+      this.element.click(() => {
+	  this.expandArticle()
+      });
   }
 
-  expandArticle() {
-    /* Using our reference to the article element, add or remove a class */
+  expandArticle(event) {
+      /* Using our reference to the article element, add or remove a class */
+      this.element.toggleClass("article-open");
   }
 }
 
@@ -36,4 +44,7 @@ class Article {
   Within .map, we create a new instance of Article passing in each article element 
   to the constructor
 */
-articles = articles.map();
+articles.map((index,element) => {
+    new Article(element);
+});
+
