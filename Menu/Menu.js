484b464aa23d21859c909b2ea80306f56d7a6425
Sam Park <sam.park.email@gmail.com>
2018-04-25 18:06:52 -0400

    finished

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -5,7 +5,7 @@ const menuButton = $(".menu-button");
 //Create a callback that will be passed to the menuButton click event listener
 //Inside this callback a class should be either added or removed from menu
 const toggleMenu = () => {
-$(menu).toggleClass("menu--open");
+$(menu).slideToggle(".menu--open");
 }
 
 
