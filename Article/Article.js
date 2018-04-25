16fc0c5313ddc97d19859deca1db22ddc556e6e4
Matthias Bakken <matthias.bakken@gmail.com>
2018-04-26 02:52:24 +0700

    Part one complete and working on part 2

--- Article/Article.js
+++ Article/Article.js
@@ -1,6 +1,5 @@
 /* Using jQuery, select all the article elements */
-let articles;
-
+let articles = $('.articles').find('.article');
 
 class Article {
   /* The constructor will take an element as it's only argument */
