e11803497ed511c3060d4bea5cab547f2172d3ab
Steve <shalomlandsman@gmail.com>
2018-04-25 18:59:27 -0400

    fixed menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,19 +2,14 @@
 const menu = $(".menu");
 const menuButton = $(".menu-button");
 
-menuButton.click(function(){
-    menu.toggleClass("menu--open");
-});
+const toggleMenu = () => {
+    menu.toggleClass('menu--open');
+}
 
-
+menuButton.click(toggleMenu);
 
 //Create a callback that will be passed to the menuButton click event listener
 //Inside this callback a class should be either added or removed from menu
-// const toggleMenu = () => {
-//      .toggleClass()
-//   $(".btn-toggle").click(function() {
-//     $(".nested").toggleClass("toggle-example");
-//   });
-// }
+
 
 //Using jQuery, add a click event to the menuButton, passing it the toggleMenu function.
