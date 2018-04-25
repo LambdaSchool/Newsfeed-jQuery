715455772b46edf181d92d95f4482eccc9eb88f5
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-04-25 17:55:08 -0400

    Completed animation goal #1 - used jQuery animate method to animate menu open and close

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -4,10 +4,25 @@ const menuButton = $('.menu-button');
 
 //Create a callback that will be passed to the menuButton click event listener
 //Inside this callback a class should be either added or removed from menu
+//Added stretch goal: animate menu open/close
 const toggleMenu = () => {
+  (menu.hasClass('menu--open')) ? animateMenuClose() : animateMenuOpen();
   menu.toggleClass('menu--open');
-}
+};
+
+const animateMenuOpen = function(){
+  menu.animate({
+    left: '0',
+    opacity: '1'
+  });
+};
+
+const animateMenuClose = function(){
+  menu.animate({
+    left: '-350px',
+    opacity: '0'
+  });
+};
 
 //Using jQuery, add a click event to the menuButton, passing it the toggleMenu function.
 menuButton.click(toggleMenu);
-
