76133cfff9fc14be50df029713a7774830d41943
Niloc92 <prestonresult@github.com>
2018-04-25 17:38:46 -0500

    Moises and Colin

--- Article/Article.js
+++ Article/Article.js
@@ -1,25 +1,30 @@
 /* Using jQuery, select all the article elements */
-let articles;
+let articles = $('.articles')
 
+articles.map(function (index, element) {
+
+})
 
 class Article {
   /* The constructor will take an element as it's only argument */
-  constructor(element) {
+  constructor(element, expandButton) {
+
+
     /* 
       We need to make sure the element being passed to us is a jQuery element.
       To do so, wrap the element in jQuery syntax. 
     */
-    this.element = $();
+    this.element = $(expandButton);
     /* 
       Using our new reference to the element, find the expand button within 
       the element and set a new property on the Article class. 
     */
-    this.expandButton;
+    this.expandButton.text('Click to Expand')
     /* 
       Using our new reference to the expandButton, use jQuery to set the inner
       text on the button. 
     */
-
+    this.expandButton.click((element) => { this.buttonClick(expandArticle) })
     /* 
       Set a click handler on the expand button (or article element), calling 
       the expandArticle method. 
