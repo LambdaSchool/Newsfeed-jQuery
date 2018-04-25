3a609a56453d4344f18cc1a3fc0d9c4080ef92e7
Zach Campbell <beazlebubbles@gmail.com>
2018-04-25 14:11:02 -0600

    Added reference to Article.js in index.html so it will actually run created Article constructor

--- Article/Article.js
+++ Article/Article.js
@@ -12,17 +12,17 @@ class Article {
       We need to make sure the element being passed to us is a jQuery element.
       To do so, wrap the element in jQuery syntax.
     */
-    this.element = $();
+    this.element = $(element);
     /*
       Using our new reference to the element, find the expand button within
       the element and set a new property on the Article class.
     */
-    this.expandButton;
+    this.expandButton = this.element.find('.expandButton');
     /*
       Using our new reference to the expandButton, use jQuery to set the inner
       text on the button.
     */
-
+    this.expandButton.text('Click to Expand');
     /*
       Set a click handler on the expand button (or article element), calling
       the expandArticle method.
@@ -41,4 +41,4 @@ class Article {
 */
 articles = articles.map(function(index, element) {
   new Article(element);
-};
+});
