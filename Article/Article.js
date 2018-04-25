a9452dd3a4535e611380678d7fc687c78e46f239
Zach Campbell <beazlebubbles@gmail.com>
2018-04-25 13:51:34 -0600

    Selected articles using jquery Mapped new article objects into articles

--- Article/Article.js
+++ Article/Article.js
@@ -1,28 +1,31 @@
 /* Using jQuery, select all the article elements */
-let articles;
+let articles = $(".article");
+
+
+
 
 
 class Article {
   /* The constructor will take an element as it's only argument */
   constructor(element) {
-    /* 
+    /*
       We need to make sure the element being passed to us is a jQuery element.
-      To do so, wrap the element in jQuery syntax. 
+      To do so, wrap the element in jQuery syntax.
     */
     this.element = $();
-    /* 
-      Using our new reference to the element, find the expand button within 
-      the element and set a new property on the Article class. 
+    /*
+      Using our new reference to the element, find the expand button within
+      the element and set a new property on the Article class.
     */
     this.expandButton;
-    /* 
+    /*
       Using our new reference to the expandButton, use jQuery to set the inner
-      text on the button. 
+      text on the button.
     */
 
-    /* 
-      Set a click handler on the expand button (or article element), calling 
-      the expandArticle method. 
+    /*
+      Set a click handler on the expand button (or article element), calling
+      the expandArticle method.
     */
   }
 
@@ -31,9 +34,11 @@ class Article {
   }
 }
 
-/* 
+/*
   Use jQuery's .map function to map over the array of jQuery elements
-  Within .map, we create a new instance of Article passing in each article element 
+  Within .map, we create a new instance of Article passing in each article element
   to the constructor
 */
-articles = articles.map();
+articles = articles.map(function(index, element) {
+  new Article(element);
+};
