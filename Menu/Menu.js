6f05d13bc4dccbe5356b42acfee293278aca2a71
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-04-25 18:11:50 -0400

    Added event to body to make menu close if user clicks anywhere on screen other than menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,28 +1,33 @@
 //Using jQuery, select the menu element and the menu-button element
-const menu = $('.menu');
-const menuButton = $('.menu-button');
+const menu = $('.menu'),
+      menuButton = $('.menu-button'),
+      body = $('body');
 
 //Create a callback that will be passed to the menuButton click event listener
 //Inside this callback a class should be either added or removed from menu
 //Added stretch goal: animate menu open/close
 const toggleMenu = () => {
   (menu.hasClass('menu--open')) ? animateMenuClose() : animateMenuOpen();
-  menu.toggleClass('menu--open');
 };
 
 const animateMenuOpen = function(){
   menu.animate({
     left: '0',
     opacity: '1'
-  });
+  }, () => menu.toggleClass('menu--open'));
 };
 
 const animateMenuClose = function(){
   menu.animate({
     left: '-350px',
     opacity: '0'
-  });
+  }, () => menu.toggleClass('menu--open'));
 };
 
 //Using jQuery, add a click event to the menuButton, passing it the toggleMenu function.
 menuButton.click(toggleMenu);
+body.click((event) => {
+  if ($(event.target).closest(menuButton).length === 0 && $(event.target).closest(menu).length === 0) {
+    if(menu.hasClass('menu--open')) animateMenuClose();
+  }
+});
