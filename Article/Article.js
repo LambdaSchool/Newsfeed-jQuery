cc2fd5522918470428ec6cf99eb1097532264e15
sagardesai90 <saagardesai@gmail.com>
2018-04-25 15:51:57 -0700

    almost done

--- Article/Article.js
+++ Article/Article.js
@@ -1,5 +1,6 @@
 /* Using jQuery, select all the article elements */
-let articles;
+let articles = $(".articles");
+
 
 
 class Article {
@@ -9,12 +10,15 @@ class Article {
       We need to make sure the element being passed to us is a jQuery element.
       To do so, wrap the element in jQuery syntax. 
     */
-    this.element = $();
+    this.element = $(element);// $('.article')
     /* 
       Using our new reference to the element, find the expand button within 
       the element and set a new property on the Article class. 
     */
-    this.expandButton;
+   //const menuButton = $(".menu-button");
+    this.expandButton = $(".expandButton");
+    this.expandButton.text( "Click to Expand" );
+    ;
     /* 
       Using our new reference to the expandButton, use jQuery to set the inner
       text on the button. 
@@ -24,16 +28,26 @@ class Article {
       Set a click handler on the expand button (or article element), calling 
       the expandArticle method. 
     */
+   this.expandButton.click(() => this.expandArticle());
   }
 
   expandArticle() {
     /* Using our reference to the article element, add or remove a class */
+    //‘Click to Expand’
+      this.element.toggleClass("article-open");
+ 
   }
-}
+
+  }
+
 
 /* 
   Use jQuery's .map function to map over the array of jQuery elements
   Within .map, we create a new instance of Article passing in each article element 
   to the constructor
 */
-articles = articles.map();
+articles = articles.map(function () {
+  let newArticle = new Article(".article");
+  return newArticle;
+});
+console.log(articles);
