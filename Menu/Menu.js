685039b9c355356e4c5d222148a13e3370694969
Marco A Guzman <marcoguzman16@gmail.com>
2018-04-25 15:01:27 -0700

    added Menu Button

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,12 +1,37 @@
 //Marco A. Guzman Pull commence.
 //Using jQuery, select the menu element and the menu-button element
-const menu;
-const menuButton;
+
+const menu = $(".menu");
+const menuButton = $(".menu-button");
+
+
 
 //Create a callback that will be passed to the menuButton click event listener
-//Inside this callback a class should be either added or removed from menu
-const toggleMenu = () => {
+$(".menu-button").click(function () {
+    alert("Dont Forget Your LambdaNews.");
+    
+    
+});
 
-}
+//Inside this callback a class should be either added or removed from menu
+
 
 //Using jQuery, add a click event to the menuButton, passing it the toggleMenu function.
+const toggleMenu = () => {
+    $(".menu").toggleClass("menu ul li");
+}
+$(".menu-button").click(toggleMenu);
+
+class MenuButton {
+    constructor(element) {
+        this.element = $(element)
+        this.element.click(() => { this.buttonClick() })
+
+    }
+    buttonClick(event) {
+        console.log("button clicked!");
+    
+
+
+
+    
