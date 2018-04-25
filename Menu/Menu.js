745c20a038612c3e3739f51ae01f234ee4b74a6d
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-04-25 18:48:24 -0400

    Menu animation

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,11 +2,16 @@
 const menu = $(".menu");
 const menuButton = $(".menu-button")
 
-//Create a callback that will be passed to the menuButton click event listener
-//Inside this callback a class should be either added or removed from menu
-const toggleMenu = () => {
-    menu.toggleClass("menu--open");
-}
+var tl = new TimelineLite({paused:true, reversed:true});
 
-//Using jQuery, add a click event to the menuButton, passing it the toggleMenu function.
-menuButton.click(toggleMenu);
+tl.staggerFromTo(menu, 1.5,
+    {rotationX:-90, transformOrigin:"50% 0%"},
+    {rotationX:0, ease:Elastic.easeOut}, 0.4)
+
+menuButton.on('click', function() {
+    
+  menu.toggleClass("menu--open");
+
+  tl.reversed() ? tl.play() : tl.pause(0).reversed(true);
+  
+});
