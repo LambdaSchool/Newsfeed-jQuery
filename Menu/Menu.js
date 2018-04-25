6f1e042166750ea7e8a9c55ac911e3b5c2b7c6f0
MacBookPro <macbookpro@MacBookPros-MacBook-Pro.local>
2018-04-25 15:46:14 -0500

    Menu Part I complete
    call back function and click event listener enabled

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -5,7 +5,12 @@ const menuButton = $('.menu-button');
 //Create a callback that will be passed to the menuButton click event listener
 //Inside this callback a class should be either added or removed from menu
 const toggleMenu = () => {
-
+    menu.toggleClass("menu--open");
 }
 
 //Using jQuery, add a click event to the menuButton, passing it the toggleMenu function.
+
+
+    menuButton.click(toggleMenu);
+
+  
