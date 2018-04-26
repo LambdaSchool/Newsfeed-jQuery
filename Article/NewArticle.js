65f7a30a98de81e2a301e4efe9b6dad3a15cf580
NandoTheessen <nando.theessen@gmail.com>
2018-04-26 15:45:20 +0200

    Added functionality:
    - Added funcitonality to submit button
    - Added function to fill ArticleObject with user input
    - Added functionality to create new Article using ArticleObject
    - Added Expand / Close functionality to new Article (works for one new article)

--- Article/NewArticle.js
+++ Article/NewArticle.js
@@ -9,10 +9,10 @@ class NewArticle {
         this.date = articleObject.date;
         this.body = articleObject.body;
         this.img = articleObject.img;
-        this.expandButton = `<span class='expandButton'>Click to Expand</span>`; // It also adds a Button and an event-listener 
-        this.title.click(() => { // to each new Article
-            this.expandArticle()
-        })
+        this.expandButton = `<span class='expandButton newExpandButton'>Click to Expand</span>`; // It also adds a Button and an event-listener 
+        // this.ex.click = function () { // to each new Article
+        //     this.expandArticle()
+        // }
     }
     expandArticle() { // function to Open / Close Article
         /* Using our reference to the article element, add or remove a class */
@@ -21,10 +21,37 @@ class NewArticle {
     }
     makeRoom() { // function to add our article to the DOM
         const newDiv = document.createElement("div") // create a new div to house our article
-        newDiv.classList += "article"; // assign needed class
+        newDiv.classList += "article newArticles"; // assign needed class
         // Insert our created Article into the created Div
-        newDiv.insertAdjacentHTML("beforeend", `${this.title} \n ${this.date} \n ${this.body} \n ${this.img} \n ${this.expandButton}`)
+        newDiv.insertAdjacentHTML("beforeend", `${this.title} \n ${this.date} \n ${this.body} \n ${this.expandButton}`)
         // Add our propped up div to the body of our DOM
-        document.body.appendChild(newDiv);
+        document.querySelector(".articles").insertAdjacentElement("beforeend", newDiv);
     }
 }
+
+// Vars to grab Submit button off the DOM & definition of Obj
+const submitBtn = $("input[type=submit]")
+
+let articleObject = {
+    title: '',
+    date: '',
+    body: ''
+}
+
+// Function grabbing new input & creating the needed Object using the user provided input
+function createObject() {
+    // let title =
+
+    articleObject.title = `<h2>${$("#title").val()}</h2>`
+    articleObject.date = `<p class="date">${$("[type=date]").val()}</p>`
+    articleObject.body = `<p>${$("#body").val()}</p>`
+}
+
+$(submitBtn).click(() => {
+    createObject();
+    const newestArticle = new NewArticle(articleObject)
+    newestArticle.makeRoom();
+    $(".newExpandButton").click(() => {
+        $(".newArticles").toggleClass("article-open")
+    })
+})
