b357b733b085df7da9b5694631b615a0a5c78c7b
masontmorrow <masontmorrow@gmail.com>
2018-04-26 02:52:15 +0200

    Styling
    -Overlay toggle in Article.js
    -Pointer hover in Article.less

--- Article/Article.js
+++ Article/Article.js
@@ -30,6 +30,7 @@ class Article {
   expandArticle() {
     /* Using our reference to the article element, add or remove a class */
     this.element.toggleClass("article-open");
+    this.element.toggleClass("overlay");
   }
 }
 
@@ -38,63 +39,7 @@ class Article {
   Within .map, we create a new instance of Article passing in each article element 
   to the constructor
 */
-// articles = articles.map(function(i, elem){
-//   new Article(elem);
-// });
-
-// articles = articles.map((_, item) => new Article(item));
 
 articles = articles.map((x, y) => {
   return new Article(y);
 })
-
-
-
-
-
-// /* Using jQuery, select all the article elements */
-// let articles = $(".article");
-// class Article {
-//   /* The constructor will take an element as it's only argument */
-//   constructor(element) {
-//     /* 
-//       We need to make sure the element being passed to us is a jQuery element.
-//       To do so, wrap the element in jQuery syntax. 
-//     */
-//     this.element = $(element);
-//     /* 
-//       Using our new reference to the element, find the expand button within 
-//       the element and set a new property on the Article class. 
-//     */
-//     // this.expandButton.click(() => {this.expandArticle()});    
-//     this.expandButton = this.element.find(".expandButton");
-
-//     /* 
-//       Using our new reference to the expandButton, use jQuery to set the inner
-//       text on the button. 
-//     */
-//     // expandArticle() {
-//     //   text()("Click to Expand")
-//     // }
-//     this.expandButton.text("Click to Expand");
-//     /* 
-//       Set a click handler on the expand button (or article element), calling 
-//       the expandArticle method. 
-//     */
-//     this.element.click(this.expandArticle);
-//   }
-
-//   expandArticle() {
-//     /* Using our reference to the article element, add or remove a class */
-//     let article = $(this);
-//     article.toggleClass("article-open");
-//   }
-// }
-// /* 
-//   Use jQuery's .map function to map over the array of jQuery elements
-//   Within .map, we create a new instance of Article passing in each article element 
-//   to the constructor
-// */
-// articles = articles.map(function (index, element) {
-//   new Article(element);
-// });
