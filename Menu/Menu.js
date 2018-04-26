bcf6581c6a0ea4d87cfd23b6bb719c692157036a
masontmorrow <masontmorrow@gmail.com>
2018-04-26 20:30:39 +0200

    Updated Menu.js
    * toggleClass --> slideToggle

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -5,8 +5,9 @@ const menuButton = $(".menu-button");
 //Create a callback that will be passed to the menuButton click event listener
 //Inside this callback a class should be either added or removed from menu
 const toggleMenu = () => {
-    menu.toggleClass("menu--open");
+    menu.slideToggle(300);
 }
 
 //Using jQuery, add a click event to the menuButton, passing it the toggleMenu function.
 menuButton.click(toggleMenu);
+
