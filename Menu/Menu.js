ad619794b4d4a6b99786e9f2904c19f9b77b5caf
jaklundun <govi.escudero@gmail.com>
2018-04-25 19:45:39 -0700

    Made updates to Menu.js

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -7,11 +7,11 @@ const $menuButton = $('.menu-button');
 
 //Inside this callback a class should be either added or removed from menu
 const toggleMenu = () => {
-    $('.menu').toggleClass('menu--open');
+    $menu.toggleClass('menu--open');
 }
 
 //Using jQuery, add a click event to the menuButton, passing it the toggleMenu function.
 
-$('.menu-button').click(toggleMenu);
+$menuButton.click(toggleMenu);
 
 
