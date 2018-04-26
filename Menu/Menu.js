709e2b976cedc2e0169cba0b45ed11a4b3c5c01c
David <AquilaVirtual>
2018-04-25 22:13:05 -0700

    update

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,16 +1,14 @@
 //Using jQuery, select the menu element and the menu-button element
 const menu = $(".menu");
-                
-//console.log(menu[0]);
+
 const menuButton = $(".menu-button");
 
 //Create a callback that will be passed to the menuButton click event listener
 //Inside this callback a class should be either added or removed from menu
-
-
 const toggleMenu = () => {
-    menu.toggleClass("menu--open");
+ menu.toggleClass("menu--open");
 }
-
 menuButton.click(toggleMenu);
+
+
 //Using jQuery, add a click event to the menuButton, passing it the toggleMenu function.
