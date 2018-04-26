d1b22681095366a759974791cf8d6742de11a734
Brandon Hopper <BrandonHopper0@gmail.com>
2018-04-26 01:59:23 -0400

    attempted .slideDoen() on Menu.js

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,6 +1,7 @@
 //Using jQuery, select the menu element and the menu-button element
 const menu = $(".menu");
 const menuButton = $(".menu-button");
+const menuSlide = $("menu-open")
 
 //Create a callback that will be passed to the menuButton click event listener
 //Inside this callback a class should be either added or removed from menu
@@ -10,4 +11,15 @@ const toggleMenu = () => {
 menuButton.click( () => {
     toggleMenu();
 });
+
 //Using jQuery, add a click event to the menuButton, passing it the toggleMenu function.
+$(document.body).click(function (){
+    menuSlide.click( () => {
+        if ( $( "menuSlide:first" ).is( ":hidden" ) ) {
+            $( "menuSlide" ).slideDown( "slow" );
+          } else {
+            $( "menuSlide" ).hide();
+          }
+    });
+})
+console.log(menuSlide)
